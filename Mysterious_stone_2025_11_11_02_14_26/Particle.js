c70ae0a51f8f55e02820dd2594478568fb7fdf36
banwol12 <gkxkddl@gmail.com>
2025-11-11 11:31:49 +0900

    클래스 분리 리팩토링

--- Mysterious_stone_2025_11_11_02_14_26/Particle.js
+++ Mysterious_stone_2025_11_11_02_14_26/Particle.js
@@ -43,26 +43,3 @@ Particle.prototype.display = function() {
 Particle.prototype.isDead = function(){
   return this.lifespan < 0;
 };
-
-
-// 여기까지 파티클 밑에부터 파티클 시스템
-
-
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
-  }
-};
